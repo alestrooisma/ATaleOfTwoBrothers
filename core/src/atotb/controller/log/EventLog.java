fec35193aeffb457b188d74f4a6ff0b54660d2bf
Ale <ale@strooisma>
2015-02-07 15:15:08 +0100

    Fixed ordering issue.

--- core/src/atotb/controller/log/EventLog.java
+++ core/src/atotb/controller/log/EventLog.java
@@ -5,43 +5,64 @@ import java.util.ListIterator;
 
 /**
  * Maintains a list of events for logging purposes.
- * 
+ *
  * @author Ale Strooisma
  */
 public class EventLog implements EventProcessor {
-    private final LinkedList<Event> events = new LinkedList<Event>();
+
+	private final LinkedList<Event> queue = new LinkedList<Event>();
+	private final LinkedList<Event> events = new LinkedList<Event>();
 	private final LinkedList<EventProcessor> listeners = new LinkedList<EventProcessor>();
-	
+	private boolean processing = false;
+
 	/**
-	 * Pushes an event to this event log. The event is added at the tail of the 
-	 * log. The event is also added to all event logs that are registered as 
+	 * Pushes an event to this event log. The event is added at the tail of the
+	 * log. The event is also added to all event logs that are registered as
 	 * listeners.
-	 * 
+	 *
 	 * @param e the new event
 	 */
 	@Override
-    public void push(Event e) {
-        events.add(e);
-		for (EventProcessor ep : listeners) {
-			ep.push(e);
+	public void push(Event e) {
+		queue.add(e);
+
+		if (!processing) {
+			// Prevent other calls to process the events
+			processing = true;
+			
+			// Process all events in the queue
+			while (!queue.isEmpty()) {
+				Event queuedEvent = queue.pollFirst();
+				
+				// Add it to the local log
+				events.add(queuedEvent);
+				
+				// Send the event to all listeners
+				for (EventProcessor ep : listeners) {
+					ep.push(queuedEvent);
+				}
+			}
+			
+			// Finished for now, allow processing for the next new event
+			processing = false;
 		}
-    }
-	
+	}
+
 	/**
-	 * Removes and returns the head of the log. In other words, it returns the 
-	 * oldest event in the log.
-	 * Note that only this log is modified, not the registered listeners.
-	 * 
+	 * Removes and returns the head of the log. In other words, it returns the
+	 * oldest event in the log. Note that only this log is modified, not the
+	 * registered listeners.
+	 *
 	 * @return the head of the log
 	 */
 	public Event pull() {
 		return events.remove();
 	}
-	
+
 	/**
-	 * Adds the given log to the registered event logs. This means that any 
+	 * Adds the given log to the registered event logs. This means that any
 	 * event added to this log will also be added to the given log.
-	 * 
+	 *
 	 * @param log
 	 */
 	public void register(EventProcessor log) {
@@ -50,7 +71,7 @@ public class EventLog implements EventProcessor {
 
 	/**
 	 * Removes the given log from the list of registered logs.
-	 * 
+	 *
 	 * @param log the log to deregister
 	 * @return true if the log is successfully deregistered
 	 */
@@ -60,7 +81,7 @@ public class EventLog implements EventProcessor {
 
 	/**
 	 * Returns an iterator over the list of events.
-	 * 
+	 *
 	 * @return a list iterator
 	 */
 	public ListIterator<Event> iterator() {
@@ -69,7 +90,7 @@ public class EventLog implements EventProcessor {
 
 	/**
 	 * Returns whether there are any events in the log.
-	 * 
+	 *
 	 * @return true if there are no events in the log
 	 */
 	public boolean isEmpty() {
